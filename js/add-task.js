7b429e41580f5708bcb20fb5307becb6a8908e1e
RogoJuerg <juergen.rogozin@gmail.com>
2022-07-04 20:26:04 +0200

    commit

---
+++ js/add-task.js
@@ -0,0 +1,204 @@
+let taskData = [];
+let check = '';
+let savedData = [];
+let user = [];
+let assignedUser = [];
+
+
+async function initAddTask() {
+    await init();
+    await loadUsers();
+    generateUserSelection();
+}
+
+
+async function loadUsers() {
+    user = users;
+}
+
+
+async function saveTickets() {
+    let json_data = {
+        "id": data.length,
+        "title": savedData[0],
+        "category": savedData[1],
+        "description": savedData[2],
+        "date": "data",
+        "dueTo": savedData[4],
+        "urgency": savedData[3],
+        "status": "backlog",
+        "assignedTo": assignedUser,
+    };
+    data.push(json_data);
+    await backend.setItem('tickets', JSON.stringify(data));
+}
+
+
+function generateUserSelection() {
+    document.getElementById('userSelection').innerHTML = ``;
+    for (let i = 0; i < user.length; i++) {
+        document.getElementById('userSelection').innerHTML += /*html*/ `
+            <div onclick="assignUser(${user[i].id})" id="userSelection${[i]}" class="user-selection-user">
+                <img class="assigned-to-user-img-user" src="${user[i].img}">
+                <span>${user[i].first_name}</span>
+                <span>${user[i].last_name}</span>
+            </div>
+        `;
+        if (assignedUser.includes(user[i])) {
+            document.getElementById(`userSelection${i}`).classList.add('user-selection-user-selected');
+        }
+
+    }
+}
+
+
+function getData() {
+    let arr = ['title', 'tasks', 'description', 'urgency', 'dueTo'];
+    for (let i = 0; i < arr.length; i++) {
+        taskData[i] = document.getElementById(arr[i]);
+        savedData.push(taskData[i].value)
+    }
+    return;
+}
+
+
+function saveData() {
+    if (check == 'success') {
+        saveTickets();
+    } else {
+        console.log('failed to save data')
+    }
+}
+
+
+function checkForm() {
+    getData();
+    let popupText = document.getElementById('popupText');
+    let popupTitle = document.getElementById('popupTitle');
+    for (let i = 0; i < taskData.length; i++) {
+        if (taskData[i].value == '') {
+            fail(i);
+            popupFailed(popupText, popupTitle);
+            check = 'failed';
+        } if (!taskData[i].value == '') {
+            success(i);
+            openPopup();
+            popupSuccess(popupText, popupTitle);
+            check = 'success';
+        }
+    }
+    saveData(taskData);
+}
+
+
+function fail(i) {
+    taskData[i].classList.remove('green-border');
+    taskData[i].classList.add('red-border');
+    return;
+}
+
+
+function success(i) {
+    taskData[i].classList.remove('red-border');
+    taskData[i].classList.add('green-border');
+}
+
+
+function popupFailed(popupText, popupTitle) {
+    popupTitle.innerHTML = `Failed!`;
+    popupTitle.classList.add('red-text');
+    popupTitle.classList.remove('green-text');
+    popupText.innerHTML = `Please fill in the missing fields!`;
+    return;
+}
+
+
+function popupSuccess(popupText, popupTitle) {
+    popupTitle.innerHTML = `Success!`;
+    popupTitle.classList.add('green-text');
+    popupTitle.classList.remove('red-text');
+    popupText.innerHTML = `Task successfully added to backlog!`;
+    return;
+}
+
+
+function openPopup() {
+    document.getElementById('popup').classList.remove('d-none');
+}
+
+
+function closePopup() {
+    document.getElementById('popup').classList.add('d-none');
+    if (check == 'success') {
+        reloadPage();
+    }
+}
+
+
+function reloadPage() {
+    document.location.reload(true);
+}
+
+
+let openCheck = false;
+function openUserSelection() {
+    if (openCheck == true) {
+        closeUserSelection();
+        openCheck = false;
+    } else {
+        document.getElementById('userSelectionContainer').classList.remove('d-none');
+        openCheck = true;
+    }
+}
+
+
+function closeUserSelection() {
+    document.getElementById('userSelectionContainer').classList.add('user-selection-container-closed');
+    setTimeout(function () {
+        document.getElementById('userSelectionContainer').classList.add('d-none');
+        document.getElementById('userSelectionContainer').classList.remove('user-selection-container-closed');
+    }, 500);
+}
+
+
+function assignUser(userId) {
+    let filtered = user.filter(function (ele) {
+        return ele.id == userId;
+    });
+
+    for (let i = 0; i < assignedUser.length; i++) {
+        let selection = assignedUser[i];
+        if (userId == selection.id) {
+            assignedUser.splice(i, 1);
+            generateAssignedUser();
+            generateUserSelection();
+            return;
+        }
+    }
+    assignedUser.push(filtered[0]);
+    generateAssignedUser();
+    generateUserSelection();
+}
+
+
+function generateAssignedUser() {
+    let assign = document.getElementById('assignedUserContainer');
+    let username;
+    clearAssignedUser();
+    if (assignedUser) {
+        for (let i = 0; i < assignedUser.length; i++) {
+            username = assignedUser[i].first_name + "<br>" + assignedUser[i].last_name;
+            assign.innerHTML += `
+            <div class="assigned-user">
+            <img src="${assignedUser[i].img}">
+            <span>${username}</span>
+            </div> 
+        `;
+        }
+    }
+}
+
+
+function clearAssignedUser() {
+    document.getElementById('assignedUserContainer').innerHTML = '';
+}
---
+++ js/board.js
@@ -0,0 +1,214 @@
+let currentDraggedStatus;
+let currentDraggedElement;
+let taskStatus = ["todo", "inprogress", "testing", "done"]
+let allTasks = data;
+let acc = document.getElementsByClassName("accordion");
+let i;
+
+
+/**
+ * This function is an onload(body) function.
+ * 
+ */
+async function initBoard() {
+    await init();
+    await loadTickets();
+    await showTickets();
+    readyForOpenTask();
+
+
+}
+
+function loadTickets() {
+    for (let i = 0; i < data.length; i++) {
+        allTasks.push(data[i]);
+
+    }
+}
+
+
+
+
+
+/**
+ * get a status from taskStatus[] and add the Tickets
+ * 
+ */
+function showTickets() {
+    for (let i = 0; i < taskStatus.length; i++) {
+        let currentStatus = taskStatus[i];
+        getTasks(currentStatus);
+
+    }
+}
+
+
+/**
+ * filter tasks by status and add in the right column
+ *
+ * @param {string} currentStatus - status for using the right tasks and div-IDs
+ */
+function getTasks(currentStatus) {
+    let tasks = allTasks.filter(t => t['status'] == currentStatus);
+    let content = document.getElementById(currentStatus + "Content");
+    content.innerHTML = ``;
+    for (let i = 0; i < tasks.length; i++) {
+        let task = tasks[i];
+        let id = task['id'];
+        content.innerHTML += addHTML(task, id,);
+        document.getElementById('ticket' + id).classList.add(checkPriority(task))
+
+
+    }
+}
+
+/**
+ * check the urgency for choosing the border-color
+ * 
+ */
+function checkPriority(currentArray) {
+    priority = currentArray['urgency'];
+    if (priority == 'low') {
+        return 'low-task';
+    }
+    if (priority == 'medium') {
+        return 'medium-task';
+    }
+    if (priority == 'high') {
+        return 'high-task';
+    }
+}
+
+
+/**
+ * add information in HTML
+ * 
+ * @param {string} id - status and arreyposition to define the ticket
+ * @param {string} currentArray - This is the current task.
+ * @returns - HTML-Code with informations from the array
+ */
+function addHTML(currentArray, id) {
+    return `
+            <div id="ticket${id}" draggable="true" ondragstart="startDragging(${id})" class="task-card">
+                <button class="accordion">
+                <div>
+
+                    <span class="category">${currentArray["category"]}</span>
+
+
+                </div>
+                    <div class="ticket-header">
+                        <h3>${currentArray["title"]}</h3>
+                        <img src="${checkAssignedTo(currentArray, id)}">
+                    </div>
+                    <span class="date">${currentArray["dueTo"]}</span>
+                </button>
+                <div class="panel">
+                <div class="arrows">
+                        <img src="src/img/arrow-icon-left.png" onclick="moveTicketLeft(${currentArray})">
+                        <img src="src/img/arrow-icon-right.png" onclick="moveTicketRight(${currentArray})">
+                    </div>
+                    <span class="description">${currentArray["description"]}</span>
+                </div>
+            </div>
+         `;
+}
+
+
+
+function checkAssignedTo(currentArray, id) {
+    let a = true;
+    if (data[id].assignedTo[0]) {
+        return data[id].assignedTo[0].img;
+    } else {
+        return "./src/img/profile.png";
+    }
+}
+
+
+
+/**
+ * get the the dragged element by array-id
+ * 
+ * @param {string} id - id from the dragged element
+ */
+function startDragging(id) {
+    currentDraggedElement = id;
+}
+
+
+function allowDrop(ev) {
+    ev.preventDefault();
+}
+
+
+/**
+ * set the new status of the ticket and update the page
+ * 
+ * @param {string} status - status for set new column
+ */
+function moveTo(status) {
+    allTasks[currentDraggedElement]['status'] = status;
+    showTickets();
+    readyForOpenTask();
+    saveData();
+}
+
+
+/**
+ * prepare the tasks for opening
+ * 
+ */
+function readyForOpenTask() {
+    for (i = 0; i < acc.length; i++) {
+        acc[i].addEventListener("click", function () {
+            /* Toggle between adding and removing the "active" class,
+            to highlight the button that controls the panel */
+            this.classList.toggle("active");
+
+            /* Toggle between hiding and showing the active panel */
+            let panel = this.nextElementSibling;
+            if (panel.style.display === "block") {
+                panel.style.display = "none";
+            } else {
+                panel.style.display = "block";
+            }
+        });
+    }
+}
+
+
+/**
+ * Safe Data[] to Backend.
+ * 
+ */
+async function saveData() {
+    await backend.setItem('tickets', JSON.stringify(data));
+}
+
+
+async function deleteTicket() {
+    await data.splice(currentDraggedElement, 1);
+    allTasks = data;
+    showTickets();
+    readyForOpenTask();
+    saveData();
+}
+
+
+//function moveTicketLeft(ticket) {
+//    for (let i = 0; i < taskStatus.length; i++) {
+//        element = taskStatus[i];
+//
+//        if (element == ticket.status) {
+//            let newIndex = i - 1;
+//            let newElement =  taskStatus[newIndex];
+//            ticket.status = newElement;
+//        }
+//        
+//    }
+//}
+
+
+
+
