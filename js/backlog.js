450c3b3881990ae35b5f05e297fb8fbe05251125
RogoJuerg <juergen.rogozin@gmail.com>
2022-07-26 19:23:04 +0200

    backlog can now move tickets ti the board

--- js/backlog.js
+++ js/backlog.js
@@ -37,12 +37,14 @@ function textShow(content, index) {
 
 
 function generateTask() {
+    document.getElementById('taskContent').innerHTML = ``;
     for (let i = 0; i < taskData.length; i++) {
-        document.getElementById('taskContent').innerHTML += taskHtml(i);
-        renderAssignedUser(i);
-        renderUserSelection(i);
+        if (taskData[i].status == "backlog") {
+            document.getElementById('taskContent').innerHTML += taskHtml(i);
+            renderAssignedUser(i);
+            renderUserSelection(i);
+        }
     }
-    
 }
 
 
@@ -54,7 +56,6 @@ function taskHtml(i) {
             <span>EXPAND TO EDIT</span>
         </div>
         <div class="ticket-user-img" id="assignedUser${i}">
-
         </div>
         <div class="ticket-title">
             <span>${taskData[i].title}</span>
@@ -69,19 +70,22 @@ function taskHtml(i) {
             <div class="expanded-user-settings">
                 <div class="user-selection-container">
                     <div id="userSelection${i}" class="user-selection-content">
-
                     </div>
                 </div>
             </div>
             <div class="ticket-options-container">
                 <div class="ticket-options">
-                    <div class="delete-img-cont" onmouseover="textShow('delete Task', ${i})" onmouseleave="textShow('choose an option')">
+                    <div class="delete-img-cont" onmouseover="textShow('delete Task', ${i})" 
+                            onmouseleave="textShow('choose an option', ${i})">
                         <img class="delete-img" src="./src/img/delete.png">
                     </div>
-                    <div class="send-to-board-img-cont" onmouseover="textShow('move to board', ${i})" onmouseleave="textShow('choose an option')">
+                    <div class="send-to-board-img-cont" onmouseover="textShow('move to board', ${i})" 
+                            onmouseleave="textShow('choose an option', ${i})"
+                            onclick="moveTaskToBoard(${i})">
                         <img class="send-to-board-img" src="./src/img/send_to_board.png">
                     </div>
-                    <div class="send-to-board-img-cont" onmouseover="textShow('save changes', ${i})" onmouseleave="textShow('choose an option')">
+                    <div class="send-to-board-img-cont" onmouseover="textShow('save changes', ${i})" 
+                            onmouseleave="textShow('choose an option', ${i})">
                         <img class="send-to-board-img" src="./src/img/save.png">
                     </div>
                 </div>
@@ -104,24 +108,46 @@ function renderUserSelection(index) {
     for (let i = 0; i < availableUsers.length; i++) {
         document.getElementById(`userSelection${index}`).innerHTML += /*html*/ `
             <div class="user-selection-user">
-                <img src="./src/img/user/img_01.png">
+                <img src="${availableUsers[i].img}">
                 <span>${availableUsers[i].first_name} ${availableUsers[i].last_name}</span>
             </div>
         `;
+        showAssignedUserInSelection(index);
     }
+
 }
 
 
 function renderAssignedUser(index) {
     let zIndex = taskData[index].assignedTo.length;
     let left = 0;
-    for (let i = 0; i < taskData[index].assignedTo.length; i++) {
-        if (taskData[index].assignedTo) {
-            document.getElementById(`assignedUser${index}`).innerHTML += /*html*/ `
-                <img style="left: ${left}px; z-index: ${zIndex};" src="${taskData[index].assignedTo[i].img}">
-        `;
-            zIndex--;
-            left += 32;
+    document.getElementById(`assignedUser${index}`).innerHTML = ``;
+    if (taskData[index].assignedTo) {
+        for (let i = 0; i < taskData[index].assignedTo.length; i++) {
+            if (taskData[index].assignedTo) {
+                document.getElementById(`assignedUser${index}`).innerHTML += /*html*/ `
+                    <img style="left: ${left}px; z-index: ${zIndex};" src="${taskData[index].assignedTo[i].img}">
+            `;
+                zIndex--;
+                left += 32;
+            }
         }
     }
+}
+
+
+function showAssignedUserInSelection(index) {
+
+}
+
+
+function moveTaskToBoard(index) {
+    taskData[index].status = 'todo';
+    saveDataToServer();
+    generateTask();
+}
+
+
+async function saveDataToServer() {
+    await backend.setItem('tickets', JSON.stringify(taskData));
 }
