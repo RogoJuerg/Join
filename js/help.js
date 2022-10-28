015717a0abb063d2849b96f391e596e1099fa2c7
Herlina Pfeiffer <pfeiffer.herlina@gmx.net>
2022-10-29 00:21:26 +0200

    help responsive

--- js/help.js
+++ js/help.js
@@ -4,7 +4,7 @@ function openBigImg(subject) {
     document.getElementById("imgBig").classList.remove("d-none");
     document.getElementById("imgBig").innerHTML = /*html*/ `
           <div onclick="closeImgBig()" class="overlay-imgBig">      
-          <img src="src/img/login.JPG" style="width:500px; cursor:pointer" alt="Log-in">
+          <img src="src/img/login.JPG" style="height:70%; cursor:pointer" alt="Log-in">
          </div>
         `;
   } else if (subject == "img2") {
@@ -21,6 +21,38 @@ function openBigImg(subject) {
     document.getElementById("imgBig").innerHTML = /*html*/ `
            <div onclick="closeImgBig()" class="overlay-imgBig">      
           <img src="src/img/addTask_resp.JPG" style="height:100%;margin-top:50px; cursor:pointer"alt="AddTask">
+          </div>
+         `;
+  } else if (subject == "img4") {
+    document.getElementById("imgBig").classList.add("overlay-bg");
+    document.getElementById("imgBig").classList.remove("d-none");
+    document.getElementById("imgBig").innerHTML = /*html*/ `
+           <div onclick="closeImgBig()" class="overlay-imgBig">      
+          <img src="src/img/backlog-big.JPG" style="height:28%;margin-top:50px; cursor:pointer"alt="Backlog">
+          </div>
+         `;
+  } else if (subject == "img5") {
+    document.getElementById("imgBig").classList.add("overlay-bg");
+    document.getElementById("imgBig").classList.remove("d-none");
+    document.getElementById("imgBig").innerHTML = /*html*/ `
+           <div onclick="closeImgBig()" class="overlay-imgBig">      
+          <img src="src/img/backlog_resp.JPG" style="height:35%;margin-top:50px; cursor:pointer"alt="Backlog">
+          </div>
+         `;
+  } else if (subject == "img6") {
+    document.getElementById("imgBig").classList.add("overlay-bg");
+    document.getElementById("imgBig").classList.remove("d-none");
+    document.getElementById("imgBig").innerHTML = /*html*/ `
+           <div onclick="closeImgBig()" class="overlay-imgBig">      
+          <img src="src/img/edit-big.JPG" style="height:40%;margin-top:50px; cursor:pointer"alt="Backlog">
+          </div>
+         `;
+  } else if (subject == "img7") {
+    document.getElementById("imgBig").classList.add("overlay-bg");
+    document.getElementById("imgBig").classList.remove("d-none");
+    document.getElementById("imgBig").innerHTML = /*html*/ `
+           <div onclick="closeImgBig()" class="overlay-imgBig">      
+          <img src="src/img/board-responsive.JPG" style="height:45%;margin-top:50px; cursor:pointer"alt="Backlog">
           </div>
          `;
   }
